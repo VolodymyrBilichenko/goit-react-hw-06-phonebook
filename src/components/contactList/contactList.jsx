5a8367468f9286e413c1f54807e99a4efcbb5cdb
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-04 13:54:04 +0200

    up pravki

--- src/components/contactList/contactList.jsx
+++ src/components/contactList/contactList.jsx
@@ -1,51 +1,38 @@
 import React from 'react';
-
 import { useSelector, useDispatch } from 'react-redux';
-
-import {deleteContact} from '../redux/createSlice';
-
+import { deleteContact } from '../redux/createSlice';
 
 const Contactslist = () => {
-const contacts = useSelector(state => state.contacts.contacts);
+  const contacts = useSelector(state => state.contacts);
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.contacts.filter);
 
+  const filteredContacts = contacts.contacts.filter(contact => {
+    return (
+      contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+      contact.number.includes(filter)
+    );
+  });
 
-const dispatch = useDispatch();
-const filter = useSelector(state => state.contactUppdate.filter);
-
-
-const filteredContacts = contacts.filter(contact => {
-  return (
-    contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-    contact.number.includes(filter)
-  );
-});
-  
-
-
-const onDeleteContact = contactId  => {
-dispatch(deleteContact(contactId));
-}
+  const onDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
 
   return (
     <>
       <ul>
-        
         {filteredContacts.map(contactitem => {
           console.log(contactitem.id);
           return (
             <li key={contactitem.id}>
               {contactitem.name} : {contactitem.number}
-              <button onClick={() => onDeleteContact(contactitem.id)} >Delete</button>
-
+              <button onClick={() => onDeleteContact(contactitem.id)}>Delete</button>
             </li>
-          )
+          );
         })}
-        
-       
       </ul>
     </>
   );
 };
 
 export default Contactslist;
-
--- src/components/redux/createSlice.js
+++ src/components/redux/createSlice.js
@@ -1,46 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {contacts: [], filter: ''}
+const initialState = { contacts: [], filter: '' };
 
 const contactsSlice = createSlice({
-
   name: 'contacts',
   initialState,
   reducers: {
-    
-      createContact: (state, action) => {
-      state.contacts.push(action.payload)
-      
+    createContact: (state, action) => {
+      state.contacts.push(action.payload);
     },
-    deleteContact: 
-    (state, action) => {
-    state.contacts = state.contacts.filter(contact => contact.id !== action.payload) 
-  }
-  }
-  
-})
-
-
-
-const contactsUppdate = createSlice({
-  name: 'contactUppdate',
-  initialState,
-  reducers: {
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
+    },
     updateFilter: (state, action) => {
-      state.filter = action.payload
-    }
-  }
-})
-
+      state.filter = action.payload;
+    },
+  },
+});
 
 export const contactReducer = contactsSlice.reducer;
-
-export const contactReducerUppdate = contactsUppdate.reducer
-
-
-
-export const { updateFilter } = contactsUppdate.actions;
-
-
-
-export const { createContact, deleteContact } = contactsSlice.actions;
+export const { createContact, deleteContact, updateFilter } = contactsSlice.actions;
